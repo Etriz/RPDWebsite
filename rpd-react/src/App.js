033aab17f0330cdc03ae15cbe0f47f5b00942d4d
ryanp <ryanpanimation@gmail.com>
2020-06-02 20:03:25 -0500

    skeleton components created

--- rpd-react/src/App.js
+++ rpd-react/src/App.js
@@ -1,7 +1,18 @@
 import React from "react";
+import Nav from "./components/Nav";
+import Header from "./components/Header";
+import Projects from "./components/Projects";
+import Footer from "./components/Footer";
 
 function App() {
-  return <div className="App">RyanPDesigns</div>;
+  return (
+    <div className="App">
+      <Nav />
+      <Header />
+      <Projects />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
---
+++ rpd-react/src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>RyanPDesigns</p>
+      <p>email</p>
+      <p>address</p>
+      <p>socials</p>
+    </footer>
+  );
+};
+
+export default Footer;
