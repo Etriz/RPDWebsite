3208c1c98c5b7dd7c1e28a457208933f4a5d00c1
ryanp <ryanpanimation@gmail.com>
2020-06-15 00:55:41 -0500

    project card layout WIP

--- rpd-react/src/components/Projects.jsx
+++ rpd-react/src/components/Projects.jsx
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
+
+import ProjectsData from "../ProjectsData.json";
 
 const Projects = () => {
+  const [projData] = useState(ProjectsData);
   return (
     <div className="projects">
-      Projects
-      <div className="card">Travel Website</div>
+      {projData.map((item) => {
+        return (
+          <div className="card" key={item.ID}>
+            <h3>{item.name}</h3>
+            <p>{item.description}</p>
+            <button>View</button>
+          </div>
+        );
+      })}
     </div>
   );
 };
